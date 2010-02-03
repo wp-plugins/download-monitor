61b1e4b717ad997e8b870b95c0d3ac51dbf9bc2f
jolley_small <jolley_small@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-03 22:40:34 +0000

    Fixed SVN
    git-svn-id: https://plugins.svn.wordpress.org/download-monitor/trunk@201609 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/jqueryFileTree/connectors/jqueryFileTreeDir.php
+++ js/jqueryFileTree/connectors/jqueryFileTreeDir.php
@@ -46,21 +46,15 @@ if( file_exists($root_dir . $_POST['dir']) ) {
 	natcasesort($files);
 	if( count($files) > 2 ) { 
 		echo "<ul class=\"jqueryFileTree\" style=\"display: none;\">";
+		echo "<li class=\"file\"><a href=\"#\" rel=\"" . htmlentities($_POST['dir']) . "\">&lt;Select this directory&gt;</a></li>";
 		// All dirs
 		foreach( $files as $file ) {
 			if( file_exists($root_dir . $_POST['dir'] . $file) && $file != '.' && $file != '..' && is_dir($root_dir . $_POST['dir'] . $file) ) {
 				echo "<li class=\"directory collapsed\"><a href=\"#\" rel=\"" . htmlentities($_POST['dir'] . $file) . "/\">" . htmlentities($file) . "</a></li>";
 			}
-		}
-		// All files
-		foreach( $files as $file ) {
-			if( file_exists($root_dir . $_POST['dir'] . $file) && $file != '.' && $file != '..' && !is_dir($root_dir . $_POST['dir'] . $file) ) {
-				$ext = preg_replace('/^.*\./', '', $file);
-				echo "<li class=\"file ext_$ext\"><a href=\"#\" rel=\"" . htmlentities($_POST['dir'] . $file) . "\">" . htmlentities($file) . "</a></li>";
-			}
-		}
+		}		
 		echo "</ul>";	
-	}
+	} 
 }
 
 ?>
